2b1d9a806fbb48a16ca782688813e12191a5f595
raketa92 <nazar.erdem@gmail.com>
2020-03-12 17:42:08 +0500

    added contact button and fixed smooth scroll

--- js/main.js
+++ js/main.js
@@ -119,7 +119,7 @@ $(document).ready(function () {
 
 
   // Select all links with hashes
-  $('.navbar-nav a[href*="#"]')
+  $('a[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
